fa0930f963658c4c488de80055c07ec8bae08ac2
Mykola Moroz <moroz.mykola@gmail.com>
2020-09-29 18:21:49 +0300

    feat: add min & max functions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 
 exports.min = function min (array) {
+  if (array != null && array.length > 0 ) {
+      let minArray = 0;
+      for (let i = 0; i < array.length; i++) {
+          if (array[i] < minArray) {
+              minArray = array[i]
+          }
+      }
+      return minArray;
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+    if (array != null && array.length > 0 ) {
+        let maxArray = 0;
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] > maxArray) {
+                maxArray = array[i]
+            }
+        }
+        return maxArray;
+    }  
   return 0;
 }
 
